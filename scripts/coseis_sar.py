ad3312502161d06d325ae205c9ac0d0787facf5a
cmspeed <cole.speed@jpl.nasa.gov>
2025-03-27 17:28:45 -0700

    Implement next_pass.find_next_overpass in forward mode

--- scripts/coseis_sar.py
+++ scripts/coseis_sar.py
@@ -16,6 +16,7 @@ from collections import defaultdict
 from itertools import combinations
 import logging
 import yagmail
+import next_pass
 
 # Set logging level to WARNING to suppress DEBUG and INFO logs
 logging.basicConfig(level=logging.WARNING)
@@ -28,6 +29,7 @@ coastline_api = "https://raw.githubusercontent.com/OSGeo/PROJ/refs/heads/master/
 ASF_DAAC_API = "https://api.daac.asf.alaska.edu/services/search/param"
 #root_dir = os.path.join(os.getcwd(), "data")  # Defaults to ./data; change is
 root_dir = '/u/trappist-r0/colespeed/work/coseis/earthquakes/'
+
 
 def get_historic_earthquake_data_single_date(eq_api, input_date):
     """
@@ -509,7 +511,7 @@ def get_path_and_frame_numbers(AOI, time):
         # Extract the path and frame numbers from the GeoJSON data
         for feature in data['features']:
             flight_direction = feature['properties']['flightDirection']
-            start_time = feature['properties']['startTime']
+            start_time = datetime.strptime(feature['properties']['startTime'], "%Y-%m-%dT%H:%M:%S.%fZ").strftime("%Y-%m-%d %H:%M:%S") + ' UTC'
             path_number = feature['properties']['pathNumber']
             frame_number = feature['properties']['frameNumber']
             path_frame_numbers[flight_direction][path_number].add((frame_number,start_time))  # Use a set to avoid duplicates
@@ -943,24 +945,20 @@ def get_next_pass(AOI, satellite="sentinel-1"):
     :param satellite: Satellite name ('sentinel-1', 'sentinel-2', or 'landsat')
     :return: Next overpass time or None if an error occurs
     """
-    min_lon, min_lat, max_lon, max_lat = AOI.bounds  # Extract bbox from AOI
+    min_lon, min_lat, max_lon, max_lat = AOI.bounds
 
     print("=========================================")
     print(f"Querying next pass for {satellite} over AOI...")
     print(f"BBOX: {min_lat}, {max_lat}, {min_lon}, {max_lon}")
     print("=========================================")
 
-    command = [
-        "python", "/u/trappist-r0/colespeed/work/next_pass/next_pass.py",
-        "--bbox", str(min_lat), str(max_lat), str(min_lon), str(max_lon),
-        "--satellite", satellite
-    ]
-
-    try:
-        result = subprocess.run(command, capture_output=True, text=True, check=True)
-
-    except subprocess.CalledProcessError as e:
-        print("Error running next_pass.py:\n", e.stderr)
+    result = next_pass.find_next_overpass(min_lat, max_lat, min_lon, max_lon, satellite)
+    
+    if result:
+        s1_next_collect_info = result.get("next_collect_info", "No collection info available")
+        return s1_next_collect_info
+    else:
+        print("No next pass data available.")
         return None
 
 
@@ -1018,11 +1016,15 @@ def main_forward(pairing_mode = None):
                         for key, value in path_frame_numbers.items()
                 )
 
+                # Run next_pass to get the next S1 overpasses
+                next_pass_info = get_next_pass(aoi, satellite="sentinel-1")
+
                 # Send the email with the formatted content
                 send_email(
                     f"Significant Earthquake: {message_dict['title']}",
                     (
                         f"Significant earthquake detected: {message_dict['title']} at {message_dict['time']} UTC\n"
+                        f"------------------------------------------------------------------------------------------------------------------------\n"
                         f"Epicenter coordinates (lat, lon): ({message_dict['coordinates'][1]}, {message_dict['coordinates'][0]})\n"
                         f"Depth: {message_dict['depth']} km\n"
                         f"For more details, visit the USGS Earthquake Hazard Portal page for this event: {message_dict['url']}\n"
@@ -1030,7 +1032,8 @@ def main_forward(pairing_mode = None):
                         f"Intersecting Sentinel-1 path and frame numbers over most recent 24-day period:\n"
                         f"{intersecting_paths_and_frames}\n"
                         f"------------------------------------------------------------------------------------------------------------------------\n"
-                        f"Next data acquisition will occur 12 days from the most recently acquired frame for each track.\n"
+                        f"Following are the next acquisition times and relative orbits for Sentinel-1 frames intersecting the earthquake AOI:\n"
+                        f"{next_pass_info}\n"
                         f"------------------------------------------------------------------------------------------------------------------------\n"
                         f"View an interactive map of intersecting Sentinel-1 frames and the earthquake epicenter location by downloading the attachment and launching in your web browser.\n"
                         f"------------------------------------------------------------------------------------------------------------------------\n"
